d79cc02e4f560bf9888401a0d87cec50633fbaa0
JasNjoku <=>
2025-11-05 16:24:01 +0000

    Create mappers

--- java-school-fullstack/src/main/java/mthree/com/fullstackschool/dao/mappers/TeacherMapper.java
+++ java-school-fullstack/src/main/java/mthree/com/fullstackschool/dao/mappers/TeacherMapper.java
@@ -9,9 +9,13 @@ public class TeacherMapper implements RowMapper<Teacher> {
     @Override
     public Teacher mapRow(ResultSet rs, int rowNum) throws SQLException {
         //YOUR CODE STARTS HERE
+        Teacher teacher = new Teacher();
+        teacher.setTeacherId(rs.getInt("tid"));
+        teacher.setTeacherFName(rs.getString("tFname"));
+        teacher.setTeacherLName(rs.getString("lName"));
+        teacher.setDept(rs.getString("dept"));
 
-
-        return null;
+        return teacher;
 
         //YOUR CODE ENDS HERE
     }
